ed474e33eb05e6d99c800dfe12b173c34853006d
buuer <hchi404@gmail.com>
2025-03-10 03:28:18 +0800

    refactor: isCastrate 判断

--- src/douban.js
+++ src/douban.js
@@ -1,7 +1,7 @@
-import { safeMatch, comporse, getNodeProps } from './tools'
+import { safeMatch, comporse, getNodeProps } from "./tools"
 
 export const getDoubanInfo = () => {
-  const infoNode = document.getElementById('info')
+  const infoNode = document.getElementById("info")
 
   const doubanId = getDoubanId()
   const imdbId = getImdbId(infoNode.innerHTML)
@@ -24,27 +24,47 @@ const getDoubanId = () =>
   safeMatch(window.location.pathname, /\/subject\/(\w+)(\/|$)/)
 
 const checkIsTvShow = () =>
-  !!(getNodeProps('#season') || getNodeProps('.episode_list'))
+  !!(getNodeProps("#season") || getNodeProps(".episode_list"))
 
-const getTitle = () => getNodeProps('[property="v:itemreviewed"]', 'innerText')
+const getTitle = () => getNodeProps('[property="v:itemreviewed"]', "innerText")
+
+/**
+ *
+ * @param {Element} node
+ * @param {string} endQuery
+ * @returns
+ */
+const getSiblings = (node, endQuery) => {
+  const collectText = (start, getNext, prepend = false) => {
+    let text = ""
+    for (let sibling = start; sibling; sibling = getNext(sibling)) {
+      if (sibling.matches?.(endQuery)) break
+      const content = sibling.textContent || ""
+      text = prepend ? content + text : text + content
+    }
+    return text
+  }
+
+  return (
+    collectText(node.previousSibling, (n) => n.previousSibling, true) +
+    node.textContent +
+    collectText(node.nextSibling, (n) => n.nextSibling)
+  )
+}
 
 const isCastrate = (el = document) => {
   const runTimeNode = el.querySelector('[property="v:runtime"')
-  const chinaRunTime = safeMatch(el.innerText, /片长:.*?(\d*)\D*?中国大陆/)
-  if (!runTimeNode) return false
-  const runTime = runTimeNode.getAttribute('content')
-  if (!runTime || !chinaRunTime) return false
-  return Number(runTime) - Number(chinaRunTime) > 0
+  return getSiblings(runTimeNode, "span.pl").includes("中国大陆")
 }
 
 const getFirstSeasonImdbId = ({ doubanId, imdbId }) => {
-  const selectNode = document.getElementById('season')
+  const selectNode = document.getElementById("season")
   if (!selectNode) return Promise.resolve(imdbId)
 
   const fsId = selectNode.firstElementChild.value
   if (fsId === doubanId) return Promise.resolve(imdbId)
 
-  return fetch('https://movie.douban.com/subject/' + fsId + '/')
+  return fetch("https://movie.douban.com/subject/" + fsId + "/")
     .then((res) => res.text())
     .then(getImdbId)
 }
